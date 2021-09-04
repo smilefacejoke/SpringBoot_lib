2482c9f55a44e9d825c4462e09c8478017e9894b
zwt <543503930@qq.com>
2021-09-04 13:48:38 +0800

    添加基础代码

---
+++ lie-web/start/src/test/java/com/herb/lie/LieWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herb.lie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LieWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
